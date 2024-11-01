bf0889c089c3e0faf48ae232c9f11e9cdf5f5146
git-marcopitra <marcomorete73@hotmail.com>
2024-11-01 04:40:20 +0100

    :sparkles: feat: add io endpoints

--- pages/api/v1/quest/swap/[io].ts
+++ pages/api/v1/quest/swap/[io].ts
@@ -1,22 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { findQuestProfile, findSwapBySymbols } from '@/server/lib/quest';
+import { findSwapBySymbols } from '@/server/lib/quest';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'GET') {
       const address = req.query.address as string;
-      const symbolIn = req.query.in as string;
-      const symbolOut = req.query.out as string;
-
-      if (!symbolIn && !symbolOut) {
-        const profile = await findQuestProfile(address);
-
-        const is_ok = profile.lastSwapAt ? 1 : 0;
-
-        res.status(200).json({ is_ok });
-        return;
-      }
+      const [symbolIn, symbolOut] = (req.query.io as string).split('-');
 
       if (symbolIn && symbolOut) {
         const quest = await findSwapBySymbols(address, symbolIn, symbolOut);
