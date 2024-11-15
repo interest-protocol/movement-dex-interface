0836acb04820181140ea2d1f788b732d3bf1a801
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-11-15 19:18:25 +0100

    :bug: fix galxe endpoints (#37)

--- server/model/quest.ts
+++ server/model/quest.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document, Model, Schema } from 'mongoose';
 
-const modelName = 'MovementQuestPorto';
+const modelName = 'InterestDEXQuestPorto';
 
 interface Coin {
   type: string;
