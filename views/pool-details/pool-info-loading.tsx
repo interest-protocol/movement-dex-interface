af41499287cbed4ea3dddd5ea6b13290f27d8196
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-11-18 17:29:24 +0100

    :bug: fix autofocus on swap (#46)

--- views/pool-details/pool-info-loading.tsx
+++ views/pool-details/pool-info-loading.tsx
@@ -2,11 +2,11 @@ import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 import { v4 } from 'uuid';
 
-import PoolDetailsCollapseItemSkeleton from './components/accordion/item-standard-skeleton';
+import PoolDetailsCollapseItemSkeleton from './pool-info/components/accordion/item-standard-skeleton';
 
 const PoolInfoLoading: FC = () => (
   <Box m="1rem">
-    {Array.from({ length: 6 }).map(() => (
+    {Array.from({ length: 7 }).map(() => (
       <PoolDetailsCollapseItemSkeleton key={v4()} />
     ))}
   </Box>
