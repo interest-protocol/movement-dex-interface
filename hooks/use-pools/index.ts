922f189a0ee6b1bc637b55154b7c1c1ca08b2266
Atila <atilaesousa@gmail.com>
2024-11-18 18:18:13 +0100

    :bug: fix: duplicated pools

--- hooks/use-pools/index.ts
+++ hooks/use-pools/index.ts
@@ -1,5 +1,6 @@
 import useSWR from 'swr';
 
+import { ISrPool } from '@/interface';
 import { useNetwork } from '@/lib/aptos-provider/network/network.hooks';
 
 export const usePools = (page: number = 1, findQuery = {}) => {
@@ -16,9 +17,16 @@ export const usePools = (page: number = 1, findQuery = {}) => {
         `https://pool-indexer-production.up.railway.app/api/pool/sr-amm?page=${page}&q=${JSON.stringify(findQuery)}&network=${network}&limit=30`
       ).then((res) => res.json?.());
 
+      const uniquePools = pools?.reduce((acc: ISrPool[], pool: ISrPool) => {
+        if (!acc.find((p) => p.poolAddress === pool.poolAddress)) {
+          acc.push(pool);
+        }
+        return acc;
+      }, []);
+
       return {
         done: true,
-        pools: pools ?? [],
+        pools: uniquePools ?? [],
         totalPages: Math.ceil(totalItems / limit),
       };
     },
