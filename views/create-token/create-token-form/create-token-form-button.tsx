7e5a70e54147d6cb628cf262531af466a5c7d312
Atila <atilaesousa@gmail.com>
2024-11-26 07:49:03 +0100

    :recycle: chore: removing create token cost message

--- views/create-token/create-token-form/create-token-form-button.tsx
+++ views/create-token/create-token-form/create-token-form-button.tsx
@@ -1,18 +1,11 @@
 import { UserTransactionResponse } from '@aptos-labs/ts-sdk';
 import { Network } from '@interest-protocol/aptos-sr-amm';
-import {
-  Box,
-  Button,
-  Theme,
-  Typography,
-  useTheme,
-} from '@interest-protocol/ui-kit';
+import { Box, Button } from '@interest-protocol/ui-kit';
 import { useAptosWallet } from '@razorlabs/wallet-kit';
 import { useState } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import invariant from 'tiny-invariant';
 
-import { DotErrorSVG } from '@/components/svg';
 import { EXPLORER_URL } from '@/constants';
 import { useDialog } from '@/hooks';
 import { useInterestDex } from '@/hooks/use-interest-dex';
@@ -35,8 +28,6 @@ const CreateTokenFormButton = () => {
     signAndSubmitTransaction,
   } = useAptosWallet();
   const { control, setValue, reset } = useFormContext<ICreateTokenForm>();
-
-  const { colors } = useTheme() as Theme;
 
   const values = useWatch({ control });
 
@@ -221,29 +212,7 @@ const CreateTokenFormButton = () => {
     });
 
   return (
-    <Box display="flex" alignItems="center" flexDirection="column">
-      <Box
-        p="s"
-        mb="m"
-        gap="s"
-        color="outline"
-        bg="lowContainer"
-        borderRadius="xs"
-        border="1px solid"
-        display="inline-flex"
-        borderColor="outline"
-        width="auto"
-      >
-        <DotErrorSVG
-          width="100%"
-          maxWidth="1rem"
-          maxHeight="1rem"
-          dotColor={colors.outlineVariant}
-        />
-        <Typography variant="label" size="medium">
-          It costs 2 MOVE to create a coin
-        </Typography>
-      </Box>
+    <Box display="flex" alignItems="center">
       <Button
         py="m"
         flex="1"
