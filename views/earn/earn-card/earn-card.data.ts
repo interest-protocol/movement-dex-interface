e3d26d65879fd6be5b510a8146986b1be7de1f33
MarioBatalha <mariobatalha9@gmail.com>
2024-11-26 13:54:55 +0000

    :sparkles: feat: add earn card mock data

---
+++ views/earn/earn-card/earn-card.data.ts
@@ -0,0 +1,32 @@
+import { BTCSVG, DefaultTokenSVG, MOVESVG, USDCSVG } from '@/components/svg';
+
+export const EARN_CARD_DATA = [
+  {
+    label: 'staked',
+    tokenName: ' MOVE token',
+    balance: 10,
+    TokenIcon: DefaultTokenSVG,
+    earnAmount: 12.0089,
+  },
+  {
+    label: 'unstaked',
+    tokenName: 'USDC',
+    balance: 100000,
+    TokenIcon: USDCSVG,
+    earnAmount: 2.089,
+  },
+  {
+    label: 'rewards',
+    tokenName: 'USDC',
+    balance: 80,
+    TokenIcon: MOVESVG,
+    earnAmount: 300.089,
+  },
+  {
+    label: 'unstaked',
+    tokenName: 'BTC',
+    balance: 30000,
+    TokenIcon: BTCSVG,
+    earnAmount: 2.089,
+  },
+];
--- views/earn/index.tsx
+++ views/earn/index.tsx
@@ -3,19 +3,16 @@ import { FC, useState } from 'react';
 import { v4 } from 'uuid';
 
 import Layout from '@/components/layout';
-import { DefaultTokenSVG } from '@/components/svg';
 
-import { EarnLabelEnum, EarnTabEnum } from './earn.types';
+import { EarnTabEnum } from './earn.types';
 import EarnCard from './earn-card';
+import { EARN_CARD_DATA } from './earn-card/earn-card.data';
 import EarnCardWrapper from './earn-card-wrapper';
 import EarnFilter from './earn-filter';
 import Header from './header';
 
 const Earn: FC = () => {
   const [tab, setTab] = useState<EarnTabEnum>(EarnTabEnum.Earn);
-  const [label, setLabel] = useState<EarnLabelEnum>(EarnLabelEnum.staked);
-
-  console.log('SetLabel >>', setLabel);
 
   return (
     <Layout>
@@ -40,18 +37,20 @@ const Earn: FC = () => {
           {tab == EarnTabEnum.Earn ? 'Earn' : 'My Position'}
         </Typography>
         <EarnCardWrapper>
-          {[1, 2, 3, 4, 5].map((item) => {
-            return (
-              <EarnCard
-                label={label}
-                tokenName="LP token"
-                key={v4()}
-                balance={item}
-                TokenIcon={DefaultTokenSVG}
-                earnAmount={12.00009}
-              />
-            );
-          })}
+          {EARN_CARD_DATA.map(
+            ({ label, tokenName, balance, TokenIcon, earnAmount }) => {
+              return (
+                <EarnCard
+                  label={label}
+                  tokenName={tokenName}
+                  key={v4()}
+                  balance={balance}
+                  TokenIcon={TokenIcon}
+                  earnAmount={earnAmount}
+                />
+              );
+            }
+          )}
         </EarnCardWrapper>
       </Box>
     </Layout>
