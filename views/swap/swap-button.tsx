9bf63d1e74bbb75d5b0eb37ab435f4df09c87a48
Atila <atilaesousa@gmail.com>
2024-11-27 12:49:35 +0100

    :bug: fix: close modal when click on got it

--- views/swap/swap-button.tsx
+++ views/swap/swap-button.tsx
@@ -28,7 +28,7 @@ const SwapButton = () => {
     signTransaction,
     signAndSubmitTransaction,
   } = useAptosWallet();
-  const { getValues, setValue, control } = useFormContext();
+  const { getValues, setValue, control, reset } = useFormContext();
 
   const error = useWatch({ control, name: 'error' });
   const valueIn = useWatch({ control, name: 'from.value' });
@@ -89,6 +89,8 @@ const SwapButton = () => {
         'explorerLink',
         EXPLORER_URL[Network.Porto](`txn/${txResult.hash}`)
       );
+
+      reset();
     } catch (e) {
       console.warn(e);
 
