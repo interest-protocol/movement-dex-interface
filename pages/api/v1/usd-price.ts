f5f5e04ecd7aa4cc2b7bd344c4b2b76510b6dd75
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-12-03 07:31:28 +0100

    ✨ Add bubbles to the swap UI (#91)

--- pages/api/v1/usd-price.ts
+++ pages/api/v1/usd-price.ts
@@ -8,7 +8,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const decimals = Number(req.query.decimals as string);
 
     const data = await fetch(
-      `https://testnet.mosaic.ag/porto/v1/quote?srcAsset=${type}&dstAsset=0x1e74c3312b1a7a08eb7cf61310787597ea6609d6d99ce86c0e48399144ea4ce9&amount=${FixedPointMath.toBigNumber(1, decimals).toString()}`
+      `https://testnet.mosaic.ag/porto/v1/quote?srcAsset=${type}&dstAsset=0x1e74c3312b1a7a08eb7cf61310787597ea6609d6d99ce86c0e48399144ea4ce9&amount=${FixedPointMath.toBigNumber(1, decimals).toString()}`,
+      {
+        headers: {
+          'x-api-key': 'tYPtSqDun-w9Yrric2baUAckKtzZh9U0',
+        },
+      }
     ).then((res) => res.json?.());
 
     return res.json(data.data.dstAmount);
